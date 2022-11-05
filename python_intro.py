b9ab852a97787c79624b5f32d85071acf54274d3
Cesmac <cesmac@academico.cesmac.edu.br>
2022-11-05 16:35:49 -0300

    first app

---
+++ python_intro.py
@@ -0,0 +1,17 @@
+
+
+
+def oi(nome):
+
+    print('oi '+ nome)
+
+
+pessoas = ['eu', 'vc','aaa','ola']
+
+
+for nome in pessoas:
+        oi(nome)
+        print('aaa')
+
+for i in range(7,9):
+    print(i)
